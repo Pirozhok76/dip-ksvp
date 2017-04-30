883f497933e322e79c89ce0e50e4ed8895a66e44
Pirozhok76 <aristetel@gmail.com>
2017-04-30 18:26:07 +0300

    mf , mz,wz for 3 and 4 zones

--- calc.py
+++ calc.py
@@ -15,14 +15,17 @@ class calcul:
     R1 = 287
     R2 = 289
     R3 = 283
+    Re = 283 #????????????????
     z1 = 1
     z2 = 1
     z3 = 1
+    ze = 1 #""" ?????????
     r1 = 1
     T1 = 300
     k1 = 1.4
     k2 = 1.33
     k3 = 1.33
+    ke = 1.33
     theta2 = np.array([1, 2, 3, 4, 5, 6])
     allM1 = np.array([0.1, 0.4, 0.8])
     a1 = np.sqrt(k1 * R1 * T1)
@@ -32,7 +35,7 @@ class calcul:
     r2s = np.array([0.6, 0.7, 0.8])
     rs = []
     Pi0 = np.array([1.5, 3, 6])
-    ksiG = 1
+    Ksig = 1
 
 
 
@@ -43,14 +46,15 @@ class calcul:
         ''' нет 3х эпюр'''
         ''' эпюры '''
         schet = 0
-        wzs = []
-        mzs = []
 
-        mfs = []
-        wfs = []
+        wzsPer = []
+        mzsPer = []
 
-        Ms = []
-        ws = []
+        mfsPer = []
+        wfsPer = []
+
+        MsPer = []
+        wsPer = []
         '''  ___  '''
 
         sqreps = (1 + eps1 ** 2)
@@ -94,7 +98,7 @@ class calcul:
                     mzm = mz1 * _mzm
 
                     mz = (a * r ** 2) + b * r + c
-                    mzs.append(mz)
+                    mzsPer.append(mz)
 
                     for p, m in enumerate(ms):
 
@@ -104,38 +108,44 @@ class calcul:
                         func2 = np.sqrt(k1 * R1 * T1 * func1)
 
                         mf = M1 / ((sqreps ** 0.5 ) * (func1 ** 0.5) * (r ** (2 * m)))
-                        mfs.append(mf)
+                        mfsPer.append(mf)
 
                         M = np.sqrt((mf ** 2) + (mz ** 2))
-                        Ms.append(M)
+                        MsPer.append(M)
 
                         wf = (M1 / ((sqreps ** 0.5) * (r ** m))) * func2
-                        wfs.append(wf)
+                        wfsPer.append(wf)
 
                         wz = mz * func2
                         schet += 1
-                        wzs.append(wz)
+                        wzsPer.append(wz)
 
                         print(schet)
 
 
     @staticmethod
-    def exial(R1, R2, R3, delta, eps1, r2s, allM1, a1, theta2, ms, k1, k2, k3, T1, z1, z2, z3, Pi0):   #приосевой вихрь
+    def axial(R1, R2, R3, Re, delta, eps1, r2s, allM1, a1, theta2, ms, k1, k2, k3, ke, T1, z1, z2, z3, ze, Pi0, Ksig):   #приосевой вихрь
 
+        mzs3zone = []
+        mzs4zone = []
+        mfsAx = []
+        wfsAx = []
 
         sqreps = (1 + eps1 ** 2)
+
         for i, r2 in enumerate(r2s):
 
             rs = np.arange(0, r2, 0.05)
 
             rg = r2
-            r3zones = np.arange(re, rg, 0.05)
 
 
             for o, pi0 in enumerate(Pi0):
 
                 for j, theta in enumerate(theta2):
+
                     m = 1 + ((math.log10(R1 / R2)) - (math.log10(theta))) * (1 / (math.log10(r2)))
+
                     ms.append(m)
 
                     for l, M1 in enumerate(allM1):
@@ -149,6 +159,7 @@ class calcul:
                         mf1 = mz1 * eps1
 
                         Piks = 1/(1 - (((M1 ** 2) * k1)/2))
+
                         Pie = Piks
 
 
@@ -165,23 +176,51 @@ class calcul:
 
                             B = ((z1 * R1)/(z2 * R2)) * (((k2 - 1) * k1 * (M1 ** 2))/(2 * gamma * k2 * sqreps *
                                                                                       (r2 ** (2 * m)) * theta2 * fi2))
+
                             C = ((k1 * (k3 - 1) * (M1 ** 2))/(2 * k3 * gamma * sqreps * fi2 * (r2 ** (2 * m))))\
                                 * ((z1 * R1)/(z3 * R3))
 
                             re = rg * ((1 - (1/C) *
                                         (1 - ((1/(Pie * (fi2 ** (k1/(k1 - 1))))) ** ((k3 - 1)/k3)))) ** 1/(2 * gamma))
 
+                            r3zones = np.arange(re, rg, 0.05)
+
+                            r4zones = np.arange(0, re, 0.05)
+
                             for u, r3zone in enumerate(r3zones):
 
+                                ksi3zone = 1 - C * (1 - ((r3zone/rg) ** (2 * gamma)))
 
-                                ksi3zone = 1 - C * (1 - ((r3zone/r2) ** (2 * gamma)))
+                                mz3zone = np.sqrt((2/k3) * (1 - (1/(Piks * (fi2 ** (k1/(k1 - 1)))
+                                                                    * (Ksig ** (k2/(k2 - 1)))
+                                                                    * (ksi3zone ** (k3/(k3 - 1)))))))
 
+                                mzs3zone.append(mz3zone)
+
+                            for y, r4zone in enumerate(r4zones):
+
+                                ksi4zone = 1 - C * (1 - ((r4zone/re) ** (2 * gamma))) # Ksi E
+
+                                D = ((z1 * R1)/(ze * Re)) * ((re/r2) ** (2 * gamma)) * ((k1 * (ke - 1) * (M1 ** 2))/
+                                                (2 * ke * gamma * sqreps * fi2 * Ksig * ksi4zone * ( r2 ** (2 * m))))
+
+                                U = 1 - D * (1 - ((r4zone/re) ** (2 * gamma)))
+
+                                mz4zone = np.sqrt((2/k3) * ((1/(Piks * (fi2 ** (k1/(k1 - 1))) * (Ksig ** (k2/(k2 - 1)))
+                                                            * (ksi4zone ** (k3/(k3 - 1))) * (U ** (ke/(ke - 1))))) - 1))
+
+                                mzs4zone.append(mz4zone)
 
                             for k, r in enumerate(rs):
 
-                                Mf = mf1 * (1/(r2 ** m)) * ((r/r2) ** gamma) * (((k1 * R1)/(k2 * R2)) ** 0.5)\
+                                MfAx = mf1 * (1/(r2 ** m)) * ((r/r2) ** gamma) * (((k1 * R1)/(k2 * R2)) ** 0.5)\
                                      * (1/((fi2 ** 0.5) * ((1 - B * (1 - ((r/r2) ** (2 * gamma)))) ** 0.5)))
 
+                                mfsAx.append(MfAx)
+
+                                WfAx = a1 * MfAx
+
+                                wfsAx.append(WfAx)
 
 
 
